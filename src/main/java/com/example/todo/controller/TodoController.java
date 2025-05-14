268018d0ae160ee9a47ed1a68168d50fa36f0704
Nguyen Luong <n.t.luong@agile-t.com>
2025-05-14 18:53:46 +0900

    First commit

---
+++ src/main/java/com/example/todo/controller/TodoController.java
@@ -0,0 +1,48 @@
+package com.example.todo.controller;
+
+import com.example.todo.entity.Todo;
+import com.example.todo.service.TodoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/todos")
+public class TodoController {
+
+    private final TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @GetMapping
+    public List<Todo> getAllTodos() {
+        return todoService.getAllTodos();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Todo> getTodoById(@PathVariable Long id) {
+        return todoService.getTodoById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Todo createTodo(@RequestBody Todo todo) {
+        return todoService.createTodo(todo);
+    }
+
+    @PutMapping("/{id}")
+    public Todo updateTodo(@PathVariable Long id, @RequestBody Todo todo) {
+        return todoService.updateTodo(id, todo);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTodo(@PathVariable Long id) {
+        if (!todoService.deleteTodo(id)) {
+            throw new RuntimeException("Todo not found");
+        }
+    }
+}
